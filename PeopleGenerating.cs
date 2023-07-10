69e1c33ebf6e24467301939a56a9ea85fd93170c
Josef Bálek <96839052+VesnickyTrombonista@users.noreply.github.com>
2023-07-11 01:30:06 +0200

    Add files via upload

---
+++ PeopleGenerating.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow;
+
+public class PeopleGenerating : MonoBehaviour
+{
+    // scaling of the station
+    private float scaleX = 0.45f;
+    private float scaleY = 0.5f;
+
+    public float spacing = 0.8f;
+    public float alignment = 3f;
+
+    private string[] peopleNames = new string[7] { "circle", "square", "triangle", "hexagon", "rectangular", "pentagon", "star" };
+    private void Start()
+    {
+
+    }
+
+
+    public void GeneratePerson(Dictionary<string, Transform> people, Transform station, Transform peopleQueue, List<string> spawnedStationsTypes)
+    {
+        Station stationQueue = station.GetComponent<Station>();
+        string stationType = stationQueue.name;
+        string peopleType = GeneratePeopleType(stationType, spawnedStationsTypes);
+        Vector3 position = GetStationPosition(station) + new Vector3(stationQueue.passengerQueue.Count * spacing - alignment, 0, 0);
+        GameObject newPerson = Instantiate(people[peopleType].gameObject, position, Quaternion.identity);
+        newPerson.transform.parent = peopleQueue;
+        newPerson.transform.localScale = new Vector3(scaleX, scaleY, 0f);
+        newPerson.GetComponent<Person>().name = peopleType;
+        
+        stationQueue.passengerQueue.Add(newPerson);
+    }
+    private string GeneratePeopleType(string stationName, List<string> spawnedStationsTypes)
+    {
+        int index = Random.Range(0, spawnedStationsTypes.Count);
+        string peopleName = peopleNames[index];
+        if (stationName == peopleName)
+        {
+            peopleName = GeneratePeopleType(stationName, spawnedStationsTypes);
+        }
+        return peopleName;
+    }
+    Vector3 GetStationPosition(Transform station)
+    {
+        return station.GetChild(0).GetComponent<Canvas>().transform.position;
+    }
+}
---
+++ Station.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI.Table;
+
+public class Station : MonoBehaviour
+{
+    public List<GameObject> passengerQueue = new List<GameObject>();
+    public Transform visibleQueue;
+    public string stationName;
+    private int capacityWithNoWaiting = 6;
+    private int maximalCapacity = 20;
+
+    public float spacing = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        CheckWaitingPeople();
+    }
+
+    /// <summary>
+    /// Checks the count of the waiting people in the queue and performs actions: timer or game over.
+    /// </summary>
+    private void CheckWaitingPeople()
+    {
+        if (visibleQueue.childCount >= capacityWithNoWaiting)
+        {
+            // start timer of the station
+        }
+        if (visibleQueue.childCount >= maximalCapacity)
+        {
+            // gameover    
+        }
+    }
+    public void AddPassenger(GameObject passenger)
+    {
+        passengerQueue.Add(passenger);
+        GameObject person = Instantiate(passenger.gameObject, visibleQueue.transform);
+        float xPos = passengerQueue.Count * spacing;
+        person.transform.position = new Vector3(xPos, 0f, 0f);
+    }
+
+    public void ProcessNextCustomer()
+    {
+        if (passengerQueue.Count > 0)
+        {
+           GameObject passenger = passengerQueue.ToArray()[0];
+           passengerQueue.Remove(passenger);
+        }
+    }
+}
